668ccf66a4771bed4f404a7b118d8908c8355236
xie1942 <80489464+xie1942@users.noreply.github.com>
2024-08-21 13:47:22 +0000

    feat: initiated the project

--- contracts/PERC20Sample.sol
+++ contracts/PERC20Sample.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "./PERC20.sol";
 
 contract PERC20Sample is PERC20 {
-    constructor() PERC20("ok", "opk") {}
+    constructor() PERC20("uj", "uj") {}
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
